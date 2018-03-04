255a6febb41486e290461f4105bb06173ac35897
Paul Esch-Laurent <herppfel@gmail.com>
2018-03-04 18:03:17 -0500

    Basic Gulp setup

---
+++ Gulpfile.js
@@ -0,0 +1,120 @@
+const gulp    = require("gulp"),
+      plugins = require("gulp-load-plugins")(),
+      server  = require("browser-sync").create(),
+      pkg     = require("./package.json"),
+      production = (
+                    process.env.NODE_ENV === "production" ||
+                    plugins.util.env.prod ||
+                    plugins.util.env.production
+                   ) ?
+                    true :
+                    false,
+      config  = {
+        autoprefixer: {
+          browsers: ["last 2 versions"]
+        },
+        browserSync: {
+          proxy: {
+            target: "http://localhost:8080/",
+          },
+          notify: false,
+          // Create a tunnel (if using `--tunnel`) with a subdomain of:
+          // 1. the first "chunk" of the package.json `name`
+          // 2. a random 6-character string appended to it
+          // Note: needs to be lowercased alphanumerics
+          tunnel: plugins.util.env.tunnel ?
+                  (pkg.name.trim().toLowerCase().split(/[^a-zA-Z0-9]/g)[0] + // [1]
+                  Math.random().toString(36).substr(2, 6)) :                 // [2]
+                  false,
+        },
+        csso: {
+          comments: false
+        },
+        imagemin: {
+          verbose: true
+        },
+        sass: {
+          outputStyle: (production) ? "compressed" : "expanded"
+        }
+};
+
+// Build the Sass
+gulp.task("build:sass", () => {
+  return gulp
+  .src("src/sass/*.scss")
+  .pipe(plugins.plumber())
+  .pipe(plugins.if(!production, plugins.sourcemaps.init()))
+  .pipe(plugins.sass.sync(config.sass))
+  .pipe(plugins.autoprefixer(config.autoprefixer))
+  .pipe(plugins.if(!production, plugins.sourcemaps.write(".")))
+  .pipe(plugins.if(production, plugins.csso(config.csso)))
+  .pipe(gulp.dest("public/css"))
+  .pipe(server.stream({ match: "**/*.css" }));
+});
+
+// Build the JS
+gulp.task("build:js", () => {
+  return gulp
+  .src("src/js/*.js")
+  .pipe(plugins.if(production, plugins.uglify()))
+  .pipe(gulp.dest("public/js"));
+});
+
+// Wrapper task to watch JS, build, and reload
+gulp.task("watch:js", ["build:js"], () => {
+  server.reload();
+});
+
+// Build images
+gulp.task("build:img", () => {
+  return gulp
+  .src("src/img/**/*.+(jpg|jpeg|gif|png|svg)")
+  .pipe(plugins.imagemin(config.imagemin))
+  .pipe(gulp.dest("public/img"));
+});
+
+// Wrapper task to watch images, build, and reload
+gulp.task("watch:img", ["build:img"], () => {
+  server.reload();
+});
+
+// Build
+gulp.task("build", ["build:sass", "build:js", "build:img"]);
+
+// Spin up server
+gulp.task("server", ["nodemon", "build"], done => {
+  server.init(config.browserSync, done);
+});
+
+// Use nodemon to watch for server changes
+gulp.task("nodemon", done => {
+  let started = false;
+
+  return plugins.nodemon({
+    script: "app.js"
+  }).on("start", () => {
+    // Avoid nodemon being started multiple times
+    if (!started) {
+      done();
+      started = true;
+    }
+  });
+});
+
+// Serve content and watch for changes
+gulp.task("serve", ["server"], () => {
+  gulp.watch("src/sass/**/*.scss", ["build:sass"]);
+  gulp.watch("src/js/**",          ["watch:js"]);
+  gulp.watch("src/img/**",         ["watch:img"]);
+  gulp.watch("views/**",           server.reload);
+  gulp.watch("{,routes/}*.js",     server.reload);
+});
+
+// Delete built files/directories
+gulp.task("clean", () => {
+  return gulp
+  .src("public", { read: false })
+  .pipe(plugins.clean({ force: true }));
+});
+
+gulp.task("default", ["serve"]);
--- README.md
+++ README.md
@@ -3,3 +3,48 @@
 Like whenisgood, but gucci.
 
 For CS3141 (Team Software Project) Spring 2018.
+
+## Setup
+
+Install:
+
+- [Node.js][nodejs] 8.x (check version with `node -v`)
+- [Yarn][yarn]
+- Gulp (install with `yarn global add gulp-cli`)
+- [EditorConfig][editorconfig] plugin for your editor/IDE
+
+1. `git clone` the repo
+2. `cd` to the root of the repo
+3. Run `yarn` to grab dependencies
+4. Create the `.env` file in the repo root
+5. Run `gulp` to start the Express server and Gulp watch tasks
+6. Code away, commit changes, and profit!
+
+### Deploying/Production
+
+Install:
+
+- PM2 (install with `yarn global add pm2`)
+
+1. `git clone` the repo
+2. `cd` to the root of the repo
+3. Run `yarn` to grab dependencies
+4. Run `gulp build --production` to build production-ready assets
+5. Run `NODE_ENV=production pm2 start app.js` to start the server with PM2
+
+> **NOTE**: Assets need to be re-built manually after pulling down changes.
+
+## Gulp Tasks
+
+There are several `gulp` tasks that can be useful when developing:
+
+- `serve` spins up a local server (is the `default` task)
+  - `--prod/--production` to serve the production assets (mimics production site)
+  - `--tunnel` to create a temporary URL via localtunnel.me (to share with someone)
+- `build` builds the assets
+  - `--prod/--production` builds for production (minify, optimize, etc.)
+- `clean` wipes the build directory
+
+[nodejs]: https://nodejs.org/en/
+[editorconfig]: http://editorconfig.org/#download
+[yarn]: https://yarnpkg.com/en/docs/install
