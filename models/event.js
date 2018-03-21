dc1f7bd78ca82863b055c90f2136047eabfc9532
Paul Esch-Laurent <herppfel@gmail.com>
2018-03-21 18:49:31 -0400

    Finish initial Event schema setup

--- models/event.js
+++ models/event.js
@@ -1,28 +1,14 @@
-const express  = require("express");
-const router   = express.Router();
-const path     = require("path");
 const mongoose = require("mongoose");
-const db       = mongoose.connection;
+const Schema   = mongoose.Schema;
 
-// db.dropDatabase();
-
-const TestSchema = new mongoose.Schema({
-  name: String,
-  age: Number
+const eventSchema = new Schema({
+  title: String,
+  startDate: Date,
+  endDate: Date,
+  granularity: Number,
+  createdBy: String,
+  invitedTo: [ String ],
+  timesSelected: [ Date ]
 });
 
-const TestModel = mongoose.model("Test", TestSchema);
-
-const test = new TestModel({ name: "Gurr", age: 22 }).save(err => {
-  if (err) throw err;
-  console.log("Saved!");
-})
-
-router.get("/test", (req, res, next) => {
-  TestModel.find({}, "name age", (err, results) => {
-    if (err) throw err;
-    res.render("test", { results });
-  });
-});
-
-module.exports = router;
+module.exports = mongoose.model("Event", eventSchema);
