7139b2c8b2e4a33beb8d36f3ca570c32554fd55f
Kristoff Arneson <kparneso@mtu.edu>
2018-03-22 19:28:52 -0400

    Fixed full calendar not allowing dates to be selected. Required an import of jQuery ui as well as a manual css override.

--- assets/js/create.js
+++ assets/js/create.js
@@ -33,52 +33,6 @@ document.getElementById("to-date").onclick = function func(){
   document.getElementById("to-datepicker").classList.toggle('fadeInLeft');
 }
 
-// **** NOTE ****
-// straight up stub code, bein a nub, but trying to get input
-// from the input to update the calendar; where I left off last
-
-// function setUpCalendar(startInput, endInput){
-//   var calendar = $('#calendar');
-//   calendar.fullCalendar({
-//     header: {
-//       left: 'prev,next today',
-//       center: 'title',
-//       right: 'agendaWeek,agendaDay'
-//     },
-//     selectable: true,
-//     defaultDate: '2018-03-18',
-//     defaultView: 'agendaWeek',
-//     minTime: "07:00:00",
-//     maxTime: "21:00:00",
-//     allDaySlot: false,
-//     editable: true,
-//     contentHeight:Function,
-//     eventLimit: true,
-//     select: function (start, end, jsEvent, view) {
-//       $('#calendar').fullCalendar('addEventSource', [{
-//         start: startInput,
-//         end: endInput,
-//         rendering: 'background',
-//         block: true,
-//       }, ]);
-//       $('#calendar').fullCalendar("unselect");
-//     },
-//     selectOverlap: function(event) {
-//     calendar.fullCalendar('unselect');
-//     return ! event.block;
-//     }
-//   });
-// }
-
-// $(document ).ready(function() { // document ready
-//   $('#from-datepicker').change(function func() {
-//   var startInput = $('#from-datepicker').val;
-//   var endInput = $('#to-datepicker').val;
-//   setUpCalendar(startInput,endInput);
-//   });
-
-// });
-
 $(document ).ready(function() { // document ready
   var calendar = $('#calendar');
     calendar.fullCalendar({
