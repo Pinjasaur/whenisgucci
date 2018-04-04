f6456e4ea013409f83e34c7842bfe089c54d2cac
Paul Esch-Laurent <herppfel@gmail.com>
2018-04-04 18:35:22 -0400

    Add CLI helper scripts to convert hashes and IDs

---
+++ cli/event/id2hash.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+require("dotenv").config()
+
+const Hashids = require("hashids");
+const hashids = new Hashids(process.env.HASHIDS_EVENT_SALT, process.env.HASHIDS_EVENT_LENGTH);
+
+if (process.argv[2])
+  console.log(hashids.encode(process.argv[2]));
