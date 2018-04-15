7b31662044edd558133569e8293c31eeec6ed50e
eleopold <esleopol@mtu.edu>
2018-04-15 14:57:27 -0400

    Redirect to response page using code from landing page

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,24 @@
 var codeInputStatus = false; // false being closed
 
 $( ".codeButton" ).click(function() {
-  if(codeInputStatus == true){
-    $(".codeInput").animate({width: '0px'}, 1000)
+  if(codeInputStatus === true){
+    $(".codeInput").animate({width: '0px' , padding: '0px'}, 1000)
+    $("#inputCode").animate({padding: '0px'},1000);
   //  $("#codeInput").animate({visibility: 'hidden'}, 2000)
     codeInputStatus = false;
   }// end if
   else{
-    $(".codeInput").animate({width: '200px'}, 1000)
+    $(".codeInput").animate({width: '100px', padding: '5px'}, 1000)
+    $("#inputCode").animate({padding: '5px'},1000);
     codeInputStatus = true;
   }// end else
 
 }); // end click function
+
+
+function submitEventCode(code){
+  if(event.keyCode === 13) {
+      url = "https://whenisgucci.com/event/" + code.value + "/respond";
+      $(location).attr('href', url)
+  }
+}
