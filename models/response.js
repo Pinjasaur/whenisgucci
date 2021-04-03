ccd81fa95d52befc113b900fbaa8aad9116d608a
Abdelrahman Ashraf <a.theashraf@gmail.com>
2021-04-03 09:10:19 +0800

    feat: 🎸 add timestamps to mongoose schema (#133)

--- models/response.js
+++ models/response.js
@@ -1,26 +1,29 @@
 const mongoose = require("mongoose");
-const Schema   = mongoose.Schema;
-const autoInc  = require("mongoose-plugin-autoinc");
+const Schema = mongoose.Schema;
+const autoInc = require("mongoose-plugin-autoinc");
 
-const responseSchema = new Schema({
-  eventID: {
-    type: Number,
-    required: [true, 'EventID required for this response.']
+const responseSchema = new Schema(
+  {
+    eventID: {
+      type: Number,
+      required: [true, "EventID required for this response."]
+    },
+    timesSelected: {
+      type: [{ startDate: Date, endDate: Date }],
+      required: [true, "Selected times required."]
+    },
+    email: {
+      type: String,
+      lowercase: true,
+      required: [true, "Responder email required."]
+    },
+    name: {
+      type: String,
+      required: [true, "Responder name required."]
+    }
   },
-  timesSelected: {
-    type: [ { startDate: Date, endDate: Date } ],
-    required: [true, 'Selected times required.']
-  },
-  email: {
-    type: String,
-    lowercase: true,
-    required: [true, 'Responder email required.']
-  },
-  name: {
-    type: String,
-    required: [true, 'Responder name required.']
-  }
-});
+  { timestamps: true }
+);
 
 responseSchema.plugin(autoInc.plugin, "Response");
 module.exports = mongoose.model("Response", responseSchema);
